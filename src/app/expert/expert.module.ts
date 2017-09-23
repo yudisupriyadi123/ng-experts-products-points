3e7d9938ec9bc45b650faf4d11c9fefda0cb572d
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2017-09-23 08:49:18 +0700

    Create ExpertModule

---
+++ src/app/expert/expert.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExpertListComponent } from './src/app/expert/expert-list/expert-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ExpertListComponent]
+})
+export class ExpertModule { }
