6901bcf0568ace92d8b5e8456d08e773742e8d25
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2017-09-24 20:11:21 +0700

    Remove Importing SharedModule from AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    SharedModule,
+    CoreModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
