8520f90b1b62a1c094af10d9d066b409526f5c8c
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2017-09-24 17:26:36 +0700

    Create CoreModule

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    NavbarComponent,
+  ],
+  declarations: [NavbarComponent]
+})
+export class CoreModule { }
+
