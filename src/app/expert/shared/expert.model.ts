5760e627181bb4bed8be01edba5b81b30391d0ee
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2017-09-25 20:33:19 +0700

    Add Expert model

---
+++ src/app/expert/shared/expert.model.ts
@@ -0,0 +1,11 @@
+export class Expert {
+  id            : number;
+  name          : string;
+  product_count : number;
+  avatar        : {
+    '32x32': string,
+    '64x64': string,
+    '176x176': string,
+  };
+}
+
