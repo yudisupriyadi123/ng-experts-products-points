aa4f7aa8f9e9aea3e2fe07ad4e5e7297fc71d9ba
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2017-09-27 16:05:52 +0700

    Add mock expert data

---
+++ src/app/core/mock-expert.ts
@@ -0,0 +1,38 @@
+import { Expert } from '../expert/shared/expert.model';
+
+export const EXPERT: Expert[] = [
+    {
+      id: 11,
+      name: 'Yudi Supriyadi',
+      product_count: 5,
+
+      avatar: {
+        '32x32': 'assets/images/profile_32x32.jpeg',
+        '64x64': 'assets/images/profile_64x64.jpeg',
+        '176x176': 'assets/images/profile_176x176.jpeg'
+      },
+    },
+    {
+      id: 12,
+      name: 'Irfan Maulana',
+      product_count: 8,
+
+      avatar: {
+        '32x32': 'assets/images/profile_32x32.jpeg',
+        '64x64': 'assets/images/profile_64x64.jpeg',
+        '176x176': 'assets/images/profile_176x176.jpeg'
+      },
+    },
+    {
+      id: 13,
+      name: 'Gatot Kaca',
+      product_count: 100,
+
+      avatar: {
+        '32x32': 'assets/images/profile_32x32.jpeg',
+        '64x64': 'assets/images/profile_64x64.jpeg',
+        '176x176': 'assets/images/profile_176x176.jpeg'
+      },
+    },
+];
+
