23792abe401aa86e9bebe52189bf7a2fcf950ca1
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2017-10-15 13:25:27 +0700

    Merge ExpertDetail with Expert model

--- src/app/expert/shared/expert.model.ts
+++ src/app/expert/shared/expert.model.ts
@@ -2,11 +2,17 @@ export class Expert {
   id            : number;
   name          : string;
   product_count : number;
+  bio           : string;
   avatar        : {
     '32x32': string,
     '64x64': string,
     '176x176': string,
   };
+  social_media : {
+    'twitter_username': string,
+    'linkedin_username': string,
+    'facebook_url': string, // profile url
+  }
 
   email         : string;
   password      : string;
